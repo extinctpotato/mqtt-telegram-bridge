715793e3226fd43b0da412304803548045064a90
Adam Olech <nddr89@gmail.com>
2019-11-22 23:36:49 +0100

    Logging change

--- tmbridge.py
+++ tmbridge.py
@@ -3,36 +3,43 @@
 import paho.mqtt.client as mqtt
 import configparser as c
 import sys
+import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 class Bridge(mqtt.Client):
     def on_connect(self, mqtcc, obj, flags, rc):
-        print("Connected with result code {}".format(rc))
+        logging.info("Connected with result code {}".format(rc))
 
     def on_message(self, mqttc, obj, msg):
         notif = msg.payload
         notif = notif.decode("utf-8")
-        print("[{}] {}".format(msg.topic, notif))
-        self.upd.bot.send_message(chat_id=870060908, text=notif)
+        logging.info("Received a message, forwarding")
+        self.upd.bot.send_message(chat_id=int(self.ini['TELEGRAM']['chat_id']), text=notif)
 
     def __init__(self, config, telegram):
         mqtt.Client.__init__(self)
         self.ini = config
-        '''self.connect(self.ini['MQTT']['host'],
-                self.ini['MQTT']['port'],
-                self.ini['MQTT']['timeout'])'''
-        self.connect("localhost", 1883, 60)
+        self.connect(self.ini['MQTT']['host'],
+                int(self.ini['MQTT']['port']),
+                int(self.ini['MQTT']['timeout']))
         self.subscribe(self.ini['MQTT']['subscribe'])
         self.upd = telegram
-        print("Aight")
 
 if __name__ == '__main__':
-    print("Starting")
+    logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.INFO)
+    logging.info("Initializing...")
     ini = c.ConfigParser()
-    ini.read(sys.argv[1])
+
+    if len(sys.argv) != 2 or not ini.read(sys.argv[1]):
+        logging.error("Invalid config, bailing out!")
+        sys.exit(0)
+
     tel = Updater(ini['TELEGRAM']['token'], use_context=True)
     tel.start_polling()
+    logging.info("Started Telegram Updater")
+
     br = Bridge(ini, tel)
-    rc = 0
-    while rc == 0:
-        rc = br.loop()
+    br.loop_start()
+    logging.info("Started MQTT client")
+
+    tel.idle()
